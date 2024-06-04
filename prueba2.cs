701ab683cd702a6258fe92a1e360e80f776b4af8
BruceWayneUwu <moises_21@live.com.mx>
2024-06-03 23:41:17 -0600

    prueba2.cs
    se creo archivo prueba 2 en cs

---
+++ prueba2.cs
@@ -0,0 +1,9 @@
+public class ConsultaDatos () {
+  public Int Id_Alumno {get; set;}
+  public string Nombre {get; set;}
+  public string Carrera {get; set;}
+
+  public ConsultaDatos(){
+    
+  }
+} 
